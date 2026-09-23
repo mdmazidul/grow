27058860a1ab9a94a39cca2e3b27d0a95e9e74d8
mdmazidul <mazidulrough@gmail.com>
2026-09-23 11:16:56 +0530

    Update site.data.js

--- site.data.js
+++ site.data.js
@@ -49,7 +49,7 @@ const SITE = {
 
   writings: [
     {
-      slug:"The God Who Watches", title:"God Who watches", category:"Note",
+      slug:"The God Who Watches", title:"God Who watches", category:"Story",
       excerpt:"Illustrating the Contradiction of omni and saviour nature of God through a story, a conversation",
       date:"2026-09", readMins:3, content:"content/writings/the_god_who_watches.html"
     }
